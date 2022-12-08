917a459caebb0da9309ee43b1202162c8c179283
tomarin <tomarint@gmail.com>
2022-12-08 13:51:50 +0900

    Show "-" if the specified day is off

--- src/foreground.ts
+++ src/foreground.ts
@@ -44,11 +44,14 @@
     if (overtime == null) {
       return;
     }
-    console.log("overtime:", overtime);
     if (overtime.textContent != "残業") {
+      console.log("overtime:", overtime);
       console.log("Unknown overtime format.")
       return;
     }
+    //
+    // Add a new column of the head of the table
+    //
     let new_th = head_th.cloneNode(true) as Element;
     head_tr.appendChild(new_th);
     const new_overtime = new_th.querySelector("div > span");
@@ -58,7 +61,7 @@
     new_overtime.textContent = "累計残業";
 
     //
-    // Add the body of the table
+    // Add a new column of the body of the table
     //
     const tbody = document.querySelector("#attendance-table-body > table > tbody");
     if (tbody == null) {
@@ -70,6 +73,9 @@
     }
     let cum_min = 0;
     trs.forEach(tr => {
+      const new_td = document.createElement("td");
+      new_td.className = "table01__cell--time";
+
       // 種別
       let text = tr.querySelector("td.table01__cell--cate > div")?.textContent;
       if (text == null) {
@@ -83,24 +89,30 @@
         return;
       }
       const worktime_min = parse_hhmm(text);
+      let overtime_min = 0;
       if (worktime_min === 0) {
         if (worktype === "全休(代休)") {
-          cum_min -= 8 * 60;
-        }
-      } else {
-        if (worktype === "所定休日出勤") {
-          cum_min += worktime_min;
-        } else {
-          cum_min += worktime_min - 8 * 60;
+          overtime_min = -8 * 60;
         }
       }
-      const cum_str = hhmm(cum_min);
-      const new_td = document.createElement("td");
-      new_td.className = "table01__cell--time";
-      new_td.innerText = cum_str;
+      else {
+        if (worktype === "所定休日出勤") {
+          overtime_min = worktime_min;
+        } else {
+          overtime_min = worktime_min - 8 * 60;
+        }
+      }
+
+      if (worktime_min === 0 && overtime_min === 0) {
+        new_td.innerText = "-";
+      } else {
+        cum_min += overtime_min;
+        const cum_str = hhmm(cum_min);
+        new_td.innerText = cum_str;
+      }
+      // console.log("worktype:", worktype, "worktime_min:", worktime_min, "overtime_min:", overtime_min, "cum_min:", cum_min)
       tr.appendChild(new_td);
     });
   }
   showCumulativeOvertimeHours();
-
 })();
